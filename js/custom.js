eee87e01239031aecec58f5da724e958868c9143
vietnc-1636 <nguyen.cong.viet@sun-asterisk.com>
2024-01-20 15:10:09 +0700

    fix: display time error in browsers of iphone

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,25 @@
 window.onload = function() {
-    countUpFromTime('2019,07,23');
+    countUpFromTime('2019-07-23');
 };
 
-function countUpFromTime(countFrom) {
+function countUpFromTime(date) {
     // Set the date we're counting up to
-    var startDate = new Date(countFrom).getTime();
+    var startDate = new Date(date);
 
     // Update the count up every 1 second
     setInterval(function() {
 
         // Get today's date and time
-        var now = new Date().getTime();
+        var now = new Date();
 
-        // Find the distance between now and the count up date
-        var distance = now - startDate;
+        // Find the tiem difference between now and the count up date
+        var timeDifference  = now - startDate;
 
         // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24)).toLocaleString();
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toLocaleString();
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toLocaleString();
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000).toLocaleString();
+        var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
         document.getElementById('days').innerHTML = days + ' Days';
         document.getElementById('timer').innerHTML = hours + ' Hour ' + minutes + ' Minutes ' + seconds + ' Seconds';
